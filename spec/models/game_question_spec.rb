3ed5b58109394b7c41e32006700e665ab50763ba
Arlantir <ror@worldoflosttime.ru>
2020-04-05 05:14:30 +0300

    add test's for user_helpers in game_question model

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -37,6 +37,24 @@ RSpec.describe GameQuestion, type: :model do
     end
   end
 
+  it 'correct .help_hash' do
+    # на фабрике у нас изначально хэш пустой
+    expect(game_question.help_hash).to eq({})
+
+    # добавляем пару ключей
+    game_question.help_hash[:some_key1] = 'blabla1'
+    game_question.help_hash['some_key2'] = 'blabla2'
+
+    # сохраняем модель и ожидаем сохранения хорошего
+    expect(game_question.save).to be_truthy
+
+    # загрузим этот же вопрос из базы для чистоты эксперимента
+    gq = GameQuestion.find(game_question.id)
+
+    # проверяем новые значение хэша
+    expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+  end
+
   # help_hash у нас имеет такой формат:
   # {
   #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
@@ -46,6 +64,8 @@ RSpec.describe GameQuestion, type: :model do
   #
 
   context 'user helpers' do
+
+    # проверяем работу помощь зала
     it 'correct audience_help' do
       expect(game_question.help_hash).not_to include(:audience_help)
 
@@ -55,6 +75,21 @@ RSpec.describe GameQuestion, type: :model do
 
       ah = game_question.help_hash[:audience_help]
       expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    # проверяем работу 50/50
+    it 'correct fifty_fifty' do
+      # сначала убедимся, в подсказках пока нет нужного ключа
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      # вызовем подсказку
+      game_question.add_fifty_fifty
+
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:fifty_fifty)
+      ff = game_question.help_hash[:fifty_fifty]
+
+      expect(ff).to include('b') # должен остаться правильный вариант
+      expect(ff.size).to eq 2 # всего должно остаться 2 варианта
     end
   end
 end
