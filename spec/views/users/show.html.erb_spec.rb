f03423e2c3b06fa0faa396d05af48ea3bc7df396
Arlantir <ror@worldoflosttime.ru>
2020-04-07 21:31:17 +0300

    bug test

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -5,26 +5,19 @@ require 'rails_helper'
 RSpec.describe "users/show", type: :view do
 
   before(:each) do
-    assign(:games, [
-      FactoryGirl.build_stubbed(:user, id: 1, name: 'Вася')
+    assign(:user, [
+      FactoryGirl.build_stubbed(:user, name: 'Вася')
     ])
     # binding.irb
-
+    render
   end
 
   # пользователь видит там свое имя
   it 'show his name' do
-    render
     expect(rendered).to match 'Вася'
   end
   # текущий пользователь (и только он) видит там кнопку для смены пароля
 
 
-
   # странице отрисовываются фрагменты с игрой
-  # it 'should ' do
-  #   stub_template('users/_game.html.erb' => 'User game goes here')
-  #
-  #   expect(page).to have_content('User game goes here')
-  # end
 end
