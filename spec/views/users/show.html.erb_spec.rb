fdbe44fc81d17e87a4fcb9df01ccebff081ddf0e
Arlantir <ror@worldoflosttime.ru>
2020-04-10 12:39:58 +0300

    fix

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 # Тест на шаблон users/show.html.erb
 
 RSpec.describe "users/show", type: :view do
-
   before(:each) do
-    assign(:user, [
+    assign(:user, (
       FactoryGirl.build_stubbed(:user, name: 'Вася')
-    ])
-    # binding.irb
+    ))
+
     render
   end
 
@@ -16,8 +15,35 @@ RSpec.describe "users/show", type: :view do
   it 'show his name' do
     expect(rendered).to match 'Вася'
   end
+
   # текущий пользователь (и только он) видит там кнопку для смены пароля
+  # describe '1' do
+  #   before(:each) do
+  #     assign(:user, (
+  #     FactoryGirl.build_stubbed(:user, name: 'Вася')
+  #     ))
+  #   end
+  #
+  #   it 'should ' do
+  #
+  #
+  #     expect(rendered).not_to match 'Петя'
+  #     expect(rendered).to match 'Вася'
+  #   end
+  # end
 
-
-  # странице отрисовываются фрагменты с игрой
+  #
+  #
+  # # странице отрисовываются фрагменты с игрой
+  # it 'should ' do
+  #
+  #   expect(rendered).to match 'Вася'
+  #   expect(rendered).to have_content 'User game goes here'
+  # end
+  #
+  # private
+  #
+  # def render_partial
+  #   render stub_template 'users/_game.html.erb' => 'User game goes here'
+  # end
 end
