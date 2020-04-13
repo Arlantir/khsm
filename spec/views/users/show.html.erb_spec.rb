376fd535b2f5574061a083d7c231dc07486f08cf
Arlantir <ror@worldoflosttime.ru>
2020-04-13 19:13:02 +0300

    add test in users_show for anonumous

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,32 +3,47 @@ require 'rails_helper'
 # Тест на шаблон users/show.html.erb
 
 RSpec.describe "users/show", type: :view do
-  before(:each) do
-    user = FactoryBot.create(:user, name: 'Вася')
-    assign(:user, user)
-    sign_in user
-    render
+  context "when user watch user's page" do
+    before(:each) do
+      user = FactoryBot.create(:user, name: 'Вася')
+      assign(:user, user)
+      sign_in user
+      render
+    end
+
+    # пользователь видит там свое имя
+    it 'show his name' do
+      expect(rendered).to match 'Вася'
+    end
+
+    # текущий пользователь (и только он) видит там кнопку для смены пароля
+    it 'current user' do
+      expect(rendered).to match 'Сменить имя и пароль'
+    end
+
+    # на странице отрисовываются фрагменты с игрой
+    it 'fragments with the game' do
+      games = FactoryBot.build_stubbed(:game, id: 1, current_level: 10, prize: 1000)
+
+      render_partial(games)
+
+      expect(rendered).to match '1'
+      expect(rendered).to match '10'
+      expect(rendered).to match '1 000 ₽'
+    end
   end
 
-  # пользователь видит там свое имя
-  it 'show his name' do
-    expect(rendered).to match 'Вася'
-  end
+  context "when anonumous watch user's page" do
+    before(:each) do
+      assign(:user, (FactoryBot.build_stubbed(:user)))
 
-  # текущий пользователь (и только он) видит там кнопку для смены пароля
-  it 'current user' do
-    expect(rendered).to match 'Сменить имя и пароль'
-  end
+      render
+    end
 
-  # на странице отрисовываются фрагменты с игрой
-  it 'fragments with the game' do
-    games = FactoryBot.build_stubbed(:game, id: 1, current_level: 10, prize: 1000)
-
-    render_partial(games)
-
-    expect(rendered).to match '1'
-    expect(rendered).to match '10'
-    expect(rendered).to match '1 000 ₽'
+    # анонимный юзер, не видит кнопки "сменить имя и пароль"
+    it 'current user' do
+      expect(rendered).to_not match 'Сменить имя и пароль'
+    end
   end
 
   private
